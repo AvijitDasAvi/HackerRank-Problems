1bd41646f9c1282fdd6afcda14591cae3b4d34bd
Avijit Das <61002222+AvijitDasAvi@users.noreply.github.com>
2023-06-12 00:25:50 +0600

    Add files via upload

---
+++ hackerrank/JavaStdInStdOut.java
@@ -0,0 +1,16 @@
+
+package hackerrank;
+
+import java.util.Scanner;
+
+public class JavaStdInStdOut {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        String myString = input.next();
+        int myInt = input.nextInt();
+        
+        System.out.println("myString is: "+myString);
+        System.out.println("myInt is: "+myInt);
+    }
+}
